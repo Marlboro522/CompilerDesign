ea39859c2975cfc26c9d85c2594f9a3fe6b805a5
Wasted_Marl <ilu.raghus@icloud.com>
2024-03-11 13:10:34 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,24 @@
+package main;
+import ADT.*;
+public class main {
+	public static void main(String[] args) {
+		String inFileAndPath = args[0];
+		String outFileAndPath = args[1];
+		System.out.println("Lexical for " + inFileAndPath);
+		boolean traceOn = true;
+		// Create a symbol table to store appropriate ident, number,string
+		// symbols found NO RESERVE WORDS GO IN THE SYMBOL TABLE!
+		SymbolTable symbolList;
+		symbolList = new SymbolTable(150);
+		Lexical myLexer = new Lexical(inFileAndPath, symbolList, traceOn);
+		Lexical.token currToken;
+		currToken = myLexer.GetNextToken();
+		while (currToken != null) {
+			System.out.println("\t" + currToken.mnemonic + " | \t" +
+		String.format("%04d", currToken.code) + " | \t" + currToken.lexeme);
+			currToken = myLexer.GetNextToken();
+			}
+		symbolList.PrintSymbolTable(outFileAndPath);
+		System.out.println("Done.");
+		}
+	}
